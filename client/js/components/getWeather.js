6ada48a673a9b719fc9946a791efb6b9a0a0cdc0
Nabegha Mazahir <mnabegha@MacBook.local>
2023-06-02 10:05:06 +0930

    Conditional added and try it button removed

--- client/js/components/getWeather.js
+++ client/js/components/getWeather.js
@@ -2,9 +2,8 @@ var x = document.querySelector("demo");
 let div = document.querySelector(".data");
 let latitude;
 let longitude;
-
-
-const apiKey = 'aaa';
+let temp;
+const apiKey = "9035fa55557e4d5490955535233105";
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
