7b9b9d02bd33e0603cf678cdec2f92a3eb9028ab
arissa y <arissa10293@gmail.com>
2023-06-02 15:56:09 +0930

    bottoms and shoes displays on the browser

--- controllers/attires_controller.js
+++ controllers/attires_controller.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 
-const Top = require('../models/top');
-const Bottom = require('../models/bottom');
-const getWeather = require('../components/getWeather');
+// const Top = require('../models/top');
+// const Bottom = require('../models/bottom');
+// const getWeather = require('../components/getWeather');
 
 
-// routes
-router.get('/', (req, res) => {
-    const { latitude, longitude } = req.query;
+// // routes
+// router.get('/', (req, res) => {
+//     const { latitude, longitude } = req.query;
 
-    getWeather(latitude, longitude)
-    .then(weather => {
-      Promise.all([
-        Top.findByWeather(weather),
-        Bottom.findByWeather(weather)
-      ])
-})
+//     getWeather(latitude, longitude)
+//     .then(weather => {
+//       Promise.all([
+//         Top.findByWeather(weather),
+//         Bottom.findByWeather(weather)
+//       ])
+// })
 
-module.exports = router;
+// module.exports = router;
--- controllers/suggestions_controller.js
+++ controllers/suggestions_controller.js
@@ -2,11 +2,25 @@ const express = require('express')
 const router = express.Router()
 
 const Top= require('../models/top')
+const Bottom= require('../models/bottom')
+const Shoe= require('../models/shoe')
 
 router.get('/top', (req, res) => {
     Top
         .findAll()
         .then(tops => res.json(tops))
+})
+
+router.get('/bottom', (req, res) =>{
+    Bottom
+        .findAll()
+        .then(bottoms => res.json(bottoms))
+})
+
+router.get('/shoe', (req, res) =>{
+    Shoe
+        .findAll()
+        .then(shoes => res.json(shoes))
 })
 
 module.exports = router
